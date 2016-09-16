433e74e424ea9ae4ab5c270bf5fb55c1939e892c
pietz <mail@plpp.de>
2016-09-16 19:50:09 +0200

    implemented multi filter

--- js/script.js
+++ js/script.js
@@ -5,9 +5,34 @@ var attr = 'data-group';    // Name of the Attribute containing the filter tags
 
 $(grid).isotope({});        // Initializes Isotope
 
-//
+// Click Handler
+var $filters = [];
+$(option).click(function(){
+    if ($(this).hasClass(active)) {
+        $(this).removeClass(active);
+        $filters = removeByName($filters, $(this).attr(attr));
+    } else {
+        $(this).addClass(active);
+        $filters.push($(this).attr(attr));
+    }
+    $(grid).isotope({filter: $filters.join().split(',').join('')});
+});
+
+function removeByName(arr) {
+    var what, a = arguments, L = a.length, ax;
+    while (L > 1 && arr.length) {
+        what = a[--L];
+        while ((ax= arr.indexOf(what)) !== -1) {
+            arr.splice(ax, 1);
+        }
+    }
+    return arr;
+};
+
+/*
 $(option).click(function(){
     $(option).removeClass(active);
     $(this).addClass(active);
     $(grid).isotope({filter: $(this).attr(attr)});
 });
+*/
