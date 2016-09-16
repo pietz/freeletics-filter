4f188e5fb0d9bd008cb9965cdd85dd41ace38ea9
pietz <mail@plpp.de>
2016-09-16 20:02:05 +0200

    added panel around buttons and changed colors

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var grid = '.grid';         // Wrapper around the results
 var option = '.option';     // Filter Option Element
-var active = 'active';      // Will be added to the active element
+var active = 'btn-warning';      // Will be added to the active element
 var attr = 'data-group';    // Name of the Attribute containing the filter tags
 
 $(grid).isotope({});        // Initializes Isotope
